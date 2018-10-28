dfd06d46f51bdaba7172b5257627cd75f582a43e
albertgoncalves <goncalves.albert@gmail.com>
2018-10-28 13:09:12 -0400

    Better example.

--- hs/hello_vector.hs
+++ hs/hello_vector.hs
@@ -4,22 +4,14 @@ import qualified Data.Vector.Unboxed as U
 import           System.Random    (randoms)
 import           System.Random.TF (seedTFGen)
 
-listRandomFloats :: Int -> [Float]
-listRandomFloats n = take n $ randoms $ seedTFGen (0, 0, 0, 0)
-
-convertToVec :: [Float] -> U.Vector Float
-convertToVec x = U.fromList x
--- not the fastest solution, but usable for now
-
--- provides the benefit of using a seed value as well as not requiring any
--- additional packages
-
-myRandVec :: U.Vector Float
-myRandVec = convertToVec $ listRandomFloats 10000
+nRandomFloats :: Int -> [Float]
+nRandomFloats n = take n $ randoms $ seedTFGen (0, 0, 0, 0)
 
 main :: IO ()
 main = do
-    print $ U.sum     myRandVec / (fromIntegral (U.length myRandVec))
-    print $ U.minimum myRandVec
-    print $ U.maximum myRandVec
-    print $ "Hello!"
+    let n = 1000
+    let x = U.fromList $ nRandomFloats n
+    print $ U.sum     x / (fromIntegral (U.length x))
+    print $ U.minimum x
+    print $ U.maximum x
+    print $ "It worked!"
