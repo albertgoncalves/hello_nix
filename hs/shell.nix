983fc89b188297d60f09bcb53bfe47925e7880cc
albertgoncalves <goncalves.albert@gmail.com>
2018-10-28 12:27:51 -0400

    Cleaner.

--- hs/shell.nix
+++ hs/shell.nix
@@ -2,29 +2,29 @@
 
 let
 
-  inherit (nixpkgs) pkgs;
+    inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, stdenv, yesod }:
-      mkDerivation {
-        pname = "hask";
-        version = "0.1.0.0";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        executableHaskellDepends = [ base
-                                     haskellPackages.vector
-                                     haskellPackages.random
-                                     haskellPackages.tf-random
-                                   ];
-        license = stdenv.lib.licenses.gpl3;
-      };
+    f = { mkDerivation, base, stdenv, yesod }:
+        mkDerivation {
+            pname = "hask";
+            version = "0.1.0.0";
+            src = ./.;
+            isLibrary = false;
+            isExecutable = true;
+            executableHaskellDepends = [ base
+                                         haskellPackages.vector
+                                         haskellPackages.random
+                                         haskellPackages.tf-random
+                                       ];
+            license = stdenv.lib.licenses.gpl3;
+        };
 
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
+    haskellPackages = if compiler == "default"
+                      then pkgs.haskellPackages
+                      else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage f {};
+    drv = haskellPackages.callPackage f {};
 
 in
 
-  if pkgs.lib.inNixShell then drv.env else drv
+    if pkgs.lib.inNixShell then drv.env else drv
