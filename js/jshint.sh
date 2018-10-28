16878b44246a0bf146ec5f88eab7fd51b6e71552
albertgoncalves <goncalves.albert@gmail.com>
2018-10-28 02:49:01 -0400

    Nice!

--- js/jshint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-npm install jshint
-npm exec jshint --version
---
+++ js/script.js
@@ -0,0 +1,10 @@
+const add = (x) => (y) => x + y;
+
+const main = () => {
+    const x  = 1;
+    const ys = [2, 3];
+
+    ys.map(add(x)).forEach((y) => console.log(y));
+};
+
+main();
