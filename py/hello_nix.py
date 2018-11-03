369150c3fc6fa1988a0694ded2d3636af3b98340
albertgoncalves <goncalves.albert@gmail.com>
2018-11-03 02:36:37 -0400

    Main.

--- py/hello_nix.py
+++ py/hello_nix.py
@@ -5,7 +5,13 @@ import sys
 import numpy  as np
 import pandas as pd
 
-print('python: {}'.format(sys.version))
 
-for name, mod in zip(['numpy', 'pandas'], [np, pd]):
-    print('{:<6}: {}'.format(name, mod.__version__))
+def main():
+    print('python: {}'.format(sys.version))
+
+    for name, mod in zip(['numpy', 'pandas'], [np, pd]):
+        print('{:<6}: {}'.format(name, mod.__version__))
+
+
+if __name__ == '__main__':
+    main()
