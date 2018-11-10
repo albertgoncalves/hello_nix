49009cf0d4a18d3464b7c20d93b105cae3df6614
albertgoncalves <goncalves.albert@gmail.com>
2018-11-10 12:27:52 -0500

    Tiny lint.

--- hs/hello_nix.hs
+++ hs/hello_nix.hs
@@ -11,7 +11,7 @@ main :: IO ()
 main = do
     let n = 1000
     let x = U.fromList $ nRandomFloats n
-    print $ U.sum     x / (fromIntegral (U.length x))
+    print $ U.sum     x / fromIntegral (U.length x)
     print $ U.minimum x
     print $ U.maximum x
-    print $ "It worked!"
+    print "It worked!"
